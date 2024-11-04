19f6e6d575389e364c114176c645ac282ea6e83c
Vyacheslav Bogdanov <sbhlrsa@gmail.com>
2024-11-04 21:26:08 +0400

    component library part 1

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="app">
+    <Search />
+    <DropdownWithSearch />
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+<script setup lang="ts">
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+import Search from './components/Search.vue';
+import DropdownWithSearch from './components/DropdownWithSearch.vue';
+
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+
 </style>
