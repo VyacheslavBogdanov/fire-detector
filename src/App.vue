3c077990bfd0a4f6a47461cddfddb88a3275dcfb
Vyacheslav Bogdanov <sbhlrsa@gmail.com>
2024-11-12 12:55:55 +0400

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
   <div id="app">
-    <DatePicker />
     <Search />
     <DropdownWithSearch />
     <CheckboxFilter />
     <RadioButtonFilter />
     <Chips />
     <ChipsFilter />
+    <DatePicker />
     <Informer />
   </div>
 </template>
 
 <script setup lang="ts">
 
-import DatePicker from './components/DatePicker.vue';
-import Search from './components/Search.vue';
-import DropdownWithSearch from './components/DropdownWithSearch.vue';
-import CheckboxFilter from './components/CheckboxFilter.vue';
-import RadioButtonFilter from './components/RadioButtonFilter.vue';
-import Chips from './components/Chips.vue';
-import Informer from './components/Informer.vue';
-import ChipsFilter from './components/ChipsFilter.vue';
+import Search from './components/Search/Search.vue';
+import DropdownWithSearch from './components/DropdownWithSearch/DropdownWithSearch.vue';
+import CheckboxFilter from './components/CheckboxFilter/CheckboxFilter.vue';
+import RadioButtonFilter from './components/RadioButtonFilter/RadioButtonFilter.vue';
+import Chips from './components/Chips/Chips.vue';
+import ChipsFilter from './components/ChipsFilter/ChipsFilter.vue';
+import DatePicker from './components/DatePicker/DatePicker.vue';
+import Informer from './components/Informer/Informer.vue';
 
 </script>
 
