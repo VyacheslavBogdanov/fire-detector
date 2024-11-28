5ef4ec9173c053d7feb58e183ed5cf7420900062
Vyacheslav Bogdanov <sbhlrsa@gmail.com>
2024-11-28 15:47:16 +0400

    project initialization

---
+++ fire-detector/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+	<div id="app">
+		<h1>FIRE DETECTOR APP INTERFACE</h1>
+	</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+h1 {
+	padding-top: 15%;
+	text-align: center;
+}
+</style>
